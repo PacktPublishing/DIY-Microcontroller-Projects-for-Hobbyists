d258ac0d656a3034e76ef4c8ac30f35954d9fe42
miguelgarciacanada <59582169+miguelgarciacanada@users.noreply.github.com>
2020-12-15 16:03:53 -0500

    Create Morse_SOS.ino

---
+++ Chapter6/Morse_SOS/Morse_SOS.ino
@@ -0,0 +1,56 @@
+/* Program that demonstrates how to show an S.O.S. Morse code message using a super bright LED connected to a Blue Pill's output port
+ * Written by Miguel Garcia-Ruiz
+ * December 10, 2020
+ * Ver. 1.0
+ */
+
+int led=PB12; // the Blue Pill's output port B12
+int dot_duration=150; //dot duration in milliseconds
+int dash_duration=dot_duration*3; //dash duration in milliseconds. A dash is three times the dot duration.
+int shortspace_duration=dot_duration; // space between letters
+int space_duration=dot_duration*7;//words are separated by a space equivalent to seven dots
+
+void setup() {
+      pinMode (led,OUTPUT);
+} 
+
+void S() { // making letter S
+          dot();
+          dot();
+          dot();
+          shortspace();
+}
+         
+void O() { // making letter O
+          dash();
+          dash();
+          dash();
+          shortspace();
+}
+         
+void shortspace() {
+          delay(shortspace_duration);
+} // a short space between letters
+
+void space() {
+          delay (space_duration);
+} //a space between words
+
+void dot() { //making a dot with a signal unit, which is short
+          digitalWrite(led,HIGH); 
+          delay (dot_duration); 
+          digitalWrite(led,LOW); 
+          delay(dot_duration); 
+}
+           
+void dash() { //making a dash with three signal units
+          digitalWrite(led,HIGH); 
+          delay(dash_duration); 
+          digitalWrite(led,LOW); 
+          delay(dash_duration); 
+}
+
+void loop() {
+          S(); O(); S(); //SOS message ("Save Our Souls") 
+          space();  // a space in between each SOS word made with seven signal units
+}
