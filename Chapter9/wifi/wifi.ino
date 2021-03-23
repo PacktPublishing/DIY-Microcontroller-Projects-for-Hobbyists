2819505052f8af40e5e1e65d4213d499ec546605
Pedro Santana <pecesama@gmail.com>
2021-03-23 05:56:51 -0600

    Chapter 9: Wi-Fi connection update

--- Chapter9/wifi/wifi.ino
+++ Chapter9/wifi/wifi.ino
@@ -3,17 +3,14 @@
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
 
   WiFi.begin("nombre-red", "contraseña-red");
 
-  Serial.print("Conectando");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
 
   Serial.print("Conectado, dirección IP: ");
   Serial.println(WiFi.localIP());
