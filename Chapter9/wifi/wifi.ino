122e86b42e5b8057bed37dfc0c4f369dd882fb3a
Pedro Santana <pecesama@gmail.com>
2021-03-23 05:25:47 -0600

    Chapter 9: Wi-Fi connection

---
+++ Chapter9/wifi/wifi.ino
@@ -0,0 +1,22 @@
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("nombre-red", "contraseña-red");
+
+  Serial.print("Conectando");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Conectado, dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
